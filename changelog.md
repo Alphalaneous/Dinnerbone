3d2d01b1c03b7e11957d868f23462fc1e069e53f
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-02-26 15:50:15 -0500

    Add a soft toggle

--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
+# 1.0.1
+- Add a soft toggle
+
 # 1.0.0
 - Initial Release
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 
 using namespace geode::prelude;
 
+bool toggled = true; //for the sake of no fields access
+
 class $modify(MyPlayerObject, PlayerObject) {
+
+    bool init(int p0, int p1, GJBaseGameLayer* p2, cocos2d::CCLayer* p3, bool p4) {
+		if (!PlayerObject::init(p0, p1, p2, p3, p4)) return false;
+		toggled = Mod::get()->getSettingValue<bool>("toggle");
+		return true;
+	}
 
 	void update(float dt) {
 		PlayerObject::update(dt);
+		if (!toggled) return;
 		int mod = -1;
 		float rot = 0;
 		if (m_isUpsideDown) mod = 1;
