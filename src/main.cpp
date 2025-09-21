67374c59cbd00ba0f8cc33a28de0898c62863ed1
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-09-21 16:23:00 -0400

    Dinnered the bone

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,70 @@
 
 using namespace geode::prelude;
 
-bool toggled = true; //for the sake of no fields access
+struct Settings {
+
+	bool m_enabled;
+	bool m_cube;
+	bool m_ship;
+	bool m_ufo;
+	bool m_ball;
+	bool m_wave;
+	bool m_robot;
+	bool m_spider;
+	bool m_swing;
+	bool m_shouldMirrorHorizontally;
+	bool m_shouldMirrorVertically;
+	float m_rotationModifier;
+
+	void loadSettings() {
+		m_enabled = Mod::get()->getSettingValue<bool>("toggle");
+		m_cube = Mod::get()->getSettingValue<bool>("cube-enable");
+		m_ship = Mod::get()->getSettingValue<bool>("ship-enable");
+		m_ufo = Mod::get()->getSettingValue<bool>("ufo-enable");
+		m_ball = Mod::get()->getSettingValue<bool>("ball-enable");
+		m_wave = Mod::get()->getSettingValue<bool>("wave-enable");
+		m_robot = Mod::get()->getSettingValue<bool>("robot-enable");
+		m_spider = Mod::get()->getSettingValue<bool>("spider-enable");
+		m_swing = Mod::get()->getSettingValue<bool>("swing-enable");
+		m_shouldMirrorHorizontally = Mod::get()->getSettingValue<bool>("horizontal-mirror");
+		m_shouldMirrorVertically = Mod::get()->getSettingValue<bool>("vertical-mirror");
+		m_rotationModifier = Mod::get()->getSettingValue<float>("rotation-modifier");
+	}
+
+	static Settings& get() {
+		static Settings settings;
+		return settings;
+	};
+
+};
+
+$execute {
+	Settings::get().loadSettings();
+	listenForAllSettingChanges([](std::shared_ptr<SettingV3>) {
+		Settings::get().loadSettings();
+	});
+}
 
 class $modify(MyPlayerObject, PlayerObject) {
 
-    bool init(int p0, int p1, GJBaseGameLayer* p2, cocos2d::CCLayer* p3, bool p4) {
-		if (!PlayerObject::init(p0, p1, p2, p3, p4)) return false;
-		toggled = Mod::get()->getSettingValue<bool>("toggle");
-		return true;
+	bool isCube() {
+		return !m_isShip && !m_isBird && !m_isBall && !m_isDart && !m_isRobot && !m_isSpider && !m_isSwing;
 	}
 
 	void update(float dt) {
 		PlayerObject::update(dt);
-		if (!toggled) return;
+		const auto& settings = Settings::get();
+		if (!settings.m_enabled) return;
+
+		if (!settings.m_cube && isCube()) return;
+		if (!settings.m_ship && m_isShip) return;
+		if (!settings.m_ufo && m_isBird) return;
+		if (!settings.m_ball && m_isBall) return;
+		if (!settings.m_wave && m_isDart) return;
+		if (!settings.m_robot && m_isRobot) return;
+		if (!settings.m_spider && m_isSpider) return;
+		if (!settings.m_swing && m_isSwing) return;
+
 		int mod = -1;
 		float rot = 0;
 		if (m_isUpsideDown) mod = 1;
@@ -23,10 +74,18 @@ class $modify(MyPlayerObject, PlayerObject) {
 			mod = -mod;
 			rot = -90;
 		}
-		if (m_isSwing) m_mainLayer->setScaleY(-1);
-		else {
-			m_mainLayer->setScaleY(mod);
+
+		if (settings.m_shouldMirrorVertically) {
+			if (m_isSwing) m_mainLayer->setScaleY(-1);
+			else {
+				m_mainLayer->setScaleY(mod);
+			}
 		}
-		m_mainLayer->setRotation(-getRotation()*2 + rot);
+
+		if (settings.m_shouldMirrorHorizontally) {
+			m_mainLayer->setScaleX(mod);
+		}
+
+		m_mainLayer->setRotation(-getRotation() + (settings.m_rotationModifier * getRotation()) + rot);
 	}
 };
